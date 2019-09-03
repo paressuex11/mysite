4b96506fe9322757415138fe4fc8860598c932f5
paressuex2001 <1138514846@qq.com>
2019-09-03 14:40:20 +0800

    0

--- jwxt/urls.py
+++ jwxt/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 
-from .views import show_course, helloworld, post_index, get_detail, create_article
+from .views import show_course, helloworld, post_index, get_detail, create_article, delete_article, update_article
 
 
 app_name = 'jwxt'
@@ -12,7 +12,9 @@ urlpatterns = [
     path('blog/', post_index, name = "blog"),
     path('blog/detail/<int:id>', get_detail, name = "detail"),
     path('blog/create', create_article, name = "create"),
-
+    path('blog/delete/<int:id>', delete_article, name='delete'),
+    path('blog/update/<int:id>', update_article, name = 'update'),
 ]
+
 
 
