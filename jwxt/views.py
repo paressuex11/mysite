bfd9ce2d7a8792f2ab124d8caaf2c95fa09a0a15
paressuex2001 <1138514846@qq.com>
2019-09-03 13:13:02 +0800

    0

--- jwxt/views.py
+++ jwxt/views.py
@@ -14,3 +14,17 @@ def post_index(request):
     content = {'articles': articles}
 
     return render(request, "blog/index.html", content)
+def get_detail(request, id):
+    import markdown
+    article = ArticlePost.objects.get(id = id)
+
+    article.body = markdown.markdown(article.body,
+        extensions=[
+        # 包含 缩写、表格等常用扩展
+        'markdown.extensions.extra',
+        # 语法高亮扩展
+        'markdown.extensions.codehilite',
+        ])
+
+    content = {'article': article}
+    return render(request, "blog/detail.html", content)
