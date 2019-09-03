449d0282626a304319c098290b042766a7dc8976
paressuex2001 <1138514846@qq.com>
2019-09-03 13:51:44 +0800

    0

--- jwxt/urls.py
+++ jwxt/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 
-from .views import show_course, helloworld, post_index, get_detail
+from .views import show_course, helloworld, post_index, get_detail, create_article
 
 
 app_name = 'jwxt'
@@ -10,7 +10,8 @@ urlpatterns = [
     path('course_table/', show_course),
     path('hello/', helloworld),
     path('blog/', post_index, name = "blog"),
-    path('blog/detail/<int:id>', get_detail, name = "detail")
+    path('blog/detail/<int:id>', get_detail, name = "detail"),
+    path('blog/create', create_article, name = "create"),
 
 ]
 
--- jwxt/views.py
+++ jwxt/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import ArticlePost
+from .forms import ArticlePostForm
 # Create your views here.
 def helloworld(request):
     return HttpResponse("hello world")
@@ -28,3 +29,29 @@ def get_detail(request, id):
 
     content = {'article': article}
     return render(request, "blog/detail.html", content)
+
+def create_article(request):
+    if (request.method == "POST"):
+        article_post_form = ArticlePostForm(data = request.POST)
+        if article_post_form.is_valid():
+            # 保存数据，但暂时不提交到数据库中
+            new_article = article_post_form.save(commit=False)
+            # 指定数据库中 id=1 的用户为作者
+            # 如果你进行过删除数据表的操作，可能会找不到id=1的用户
+            # 此时请重新创建用户，并传入此用户的id
+            new_article.author = User.objects.get(id=1)
+            # 将新文章保存到数据库中
+            new_article.save()
+            # 完成后返回到文章列表
+            return redirect("jwxt:blog")
+        # 如果数据不合法，返回错误信息
+        else:
+            return HttpResponse("表单内容有误，请重新填写。")
+    # 如果用户请求获取数据
+    else:
+        # 创建表单类实例
+        article_post_form = ArticlePostForm()
+        # 赋值上下文
+        context = { 'article_post_form': article_post_form }
+        # 返回模板
+        return render(request, 'blog/create.html', context)
